2f9637d890ac26ad5ee60f7f46d64f5425e41ea1
Chase Lonas <chaselonas@gmail.com>
2024-02-17 13:17:34 -0500

    seeing if the netlify adapter works

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+import netlify from '@astrojs/netlify/static';
 
+// https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   integrations: [tailwind(), alpinejs()]
 });
--- tech/astro.config.mjs
+++ tech/astro.config.mjs
@@ -5,5 +5,6 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   integrations: [tailwind(), alpinejs()]
 });
